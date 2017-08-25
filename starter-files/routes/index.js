16ab5c453a4b438e38d1496a8f9b380db9b248db
Oldřich Kruchňa <oldrichkruchna@Oldrich-MacBook-Pro.local>
2017-08-25 16:11:17 +0200

    finish templating

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -3,7 +3,23 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  console.log('Hey');
+
+  const olda = { name: 'Olda', age: 26, cool: 'yes a lot'};
+  // res.send('<h1>Hey! It works!</h1>');
+  res.json(req.query);
 });
 
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
+});
+
+router.get('/hello/:name/:dog', (req, res) => {
+  res.render('helloExtends', {
+    name: req.params.name,
+    dog: req.params.dog
+  });
+  // res.send('<h1>Hello!</h1>');
+});
 module.exports = router;
