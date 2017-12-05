cd19ce681fc2de359a0e9be76d4213275ed7c3cb
Oldrich Kruchna <kruchna.o@gmail.com>
2017-12-05 16:42:30 +0100

    add store creating, flash messages

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,13 +1,18 @@
-exports.myMiddleware = (req, res, next) => {
-    req.name = "Olda";
-    if(req.name === 'Olda') {
-        throw Error('Tuc tuc');
-    }
-    next();
-}
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
     console.log(req.name);
     res.cookie('Olda', 'is also cool', { maxAge: 900000 });
     res.render('index');
 };
+
+exports.addStore = (req, res) => {
+    res.render('editStore', { title: 'Add store'} );
+}
+
+exports.createStore = async (req, res) => {
+    const store = await (new Store(req.body)).save();
+    req.flash('success', `Store ${store.name} successfully created!`);
+    res.redirect(`store/${store.slug}`);
+}
