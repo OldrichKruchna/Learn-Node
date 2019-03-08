89c12c01e29adbfaba205d2b38cb7d12d07aa821
Oldrich Kruchna <oldrich.kruchna@sinnerschrader.com>
2019-03-08 14:34:27 -0100

    finish episode 14

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -21,3 +21,20 @@ exports.getStores = async (req, res) => {
     const stores = await Store.find();
     res.render('stores', { title: 'Stores', stores });
 }
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({ _id: req.params.id });
+
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+}
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return the new updated store instead of the old one
+    runValidators: true // force model to run validators, which are normally run only on init
+  }).exec();
+  
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View store -></a>`);
+  
+  res.redirect(`/stores/${store._id}/edit`);
+}
